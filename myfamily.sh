884897e7c32bee4352c2b7f2d9d996ad6bd52dda
aotokpa <andrewokibeotokpa@gmail.com>
2025-05-18 20:35:16 +0100

    Add myfamily.sh script

---
+++ myfamily.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+if [ -z "$HERO_ID" ]; then
+  echo "Error: HERO_ID environment variable is not set."
+  exit 1
+fi
+URL="https://acad.learn2earn.org.ng/assets/superhero/all.json"
+DATA=$(curl -s "$URL")
+RELATIVES=$(echo "$DATA" | jq -r --arg ID "$HERO_ID" '.[] | select(.id == ($ID | tonumber)) | .relatives')
+if [ -z "$RELATIVES" ]; then
+  echo "No relatives found for HERO_ID=$HERO_ID"
+else
+  echo "Relatives of HERO_ID=$HERO_ID:"
+  echo "$RELATIVES"
+fi
