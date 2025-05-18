345a8702a421673f4a92beae888e296e8312ea35
Andrew Otokpa <aotokpa@noreply.acad.learn2earn.org.ng>
2025-05-18 00:02:46 +0000

    Update to-git-or-not-to-git.sh

--- to-git-or-not-to-git.sh
+++ to-git-or-not-to-git.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -s https://acad.learn2earn.org.ng/assets/superhero/all.json \ | jq '.[] | select(.id == 170)'
+curl -s https://acad.learn2earn.org.ng/assets/superhero/all.json | jq '.[] | select(.id == 170)'
