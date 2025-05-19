e7b12445aec40b6f4bae0db7849660ac2dc5101e
aotokpa <andrewokibeotokpa@gmail.com>
2025-05-19 17:25:05 +0100

    print alphabet

--- printalphabet/main.go
+++ printalphabet/main.go
@@ -3,11 +3,11 @@ package main
 import "github.com/01-edu/z01"
 
 func main() {
-
+	// loop through the ASCII values of 'a' to 'z'
 	for r := 'a'; r <= 'z'; r++ {
-
+		// print each character using z01.PrintRune
 		z01.PrintRune(r)
 	}
-
+	// ptionally add a newline after printing all characters
 	z01.PrintRune('\n')
 }
