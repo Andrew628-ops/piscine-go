8ad4db591575ee4fa7417c2c280571528b1ae627
aotokpa <andrewokibeotokpa@gmail.com>
2025-05-20 13:06:38 +0100

    my divmod

--- divmod.go
+++ divmod.go
@@ -1,6 +1,6 @@
 package piscine
 
-func DivMod(a init, b init, div *init, mod *init)  {
+func DivMod(a init, b init, div *init, mod *init) {
 	*div = a / b // store the quotient in the variable pointed by div
 	*mod = a % b // store the remainder in the variable pointed by mod
 }
