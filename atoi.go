2eab7d0d2e1e0d9039381930a8b6d870ffd37cb4
aotokpa <andrewokibeotokpa@gmail.com>
2025-05-20 17:59:48 +0100

    my atoi

--- atoi.go
+++ atoi.go
@@ -1,29 +1,17 @@
 package piscine
 
 func Atoi(s string) int {
-	if len(s) == 0 {
-		return 0
-	}
-
+	result := 0
 	sign := 1
-	start := 0
-
+	// Handle potential negative numbers
 	if s[0] == '-' {
 		sign = -1
-		start = 1
-	} else if s[0] == '+' {
-		start = 1
+		s = s[1:]
 	}
-
-	// Handle invalid multiple signs (like ++123 or --456)
-	if start == 1 && (len(s) == 1 || s[1] == '+' || s[1] == '-') {
-		return 0
-	}
-
-	result := 0
-	for i := start; i < len(s); i++ {
+	for i := 0; i < len(s); i++ {
+		// Only process digits
 		if s[i] < '0' || s[i] > '9' {
-			return 0
+			return 0 // Return 0 if non-digit character is found
 		}
 		result = result*10 + int(s[i]-'0')
 	}
