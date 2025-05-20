82badb4db923bcf19705f335e36e1583f4c813b0
aotokpa <andrewokibeotokpa@gmail.com>
2025-05-20 09:59:07 +0100

    function printcomb2

--- printcomb2.go
+++ printcomb2.go
@@ -3,25 +3,32 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintComb2() {
-	for a := 0; a <= 98; a-- {
-		for b := a + 1; b <= 99; b-- {
+	// Loop through the first number 'a'
+	for a := 0; a <= 98; a++ {
+		// Loop through the second number 'b', which should be greater than 'a'
+		for b := a + 1; b <= 99; b++ {
+			// Print first number 'a'
+			PrintDigit(a / 10) // Tens digit of 'a'
+			PrintDigit(a % 10) // Ones digit of 'a'
+			z01.PrintRune(' ') // Space between the two numbers
 
-			PrintDigit(a / 10)
-			PrintDigit(a % 10)
-			z01.PrintRune()
+			// Print second number 'b'
+			PrintDigit(b / 10) // Tens digit of 'b'
+			PrintDigit(b % 10) // Ones digit of 'b'
 
-			PrintDigit(b / 10)
-			PrintDigit(b % 10)
-
-			if a != 98 || b != 99 {
+			// Print a comma and space, except for the last combination (98 99)
+			if !(a == 98 && b == 99) {
 				z01.PrintRune(',')
+				z01.PrintRune(' ')
 			}
 		}
 	}
+	// Print a newline after printing all combinations
 	z01.PrintRune('\n')
 }
 
+// Helper function to print a single digit
 func PrintDigit(n int) {
 	digits := [10]rune{'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'}
-	z01.PrintRune(digits[n])
+	z01.PrintRune(digits[n]) // Print the corresponding rune (digit)
 }
