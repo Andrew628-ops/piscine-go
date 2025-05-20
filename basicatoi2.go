57336d84284268a939c04004c61fd3ab014a95f5
aotokpa <andrewokibeotokpa@gmail.com>
2025-05-20 17:00:17 +0100

    my basicatoi2

--- basicatoi2.go
+++ basicatoi2.go
@@ -3,6 +3,10 @@ package piscine
 func BasicAtoi2(s string) int {
 	result := 0
 	for _, char := range s {
+		// Check if the character is a digit
+		if char < '0' || char > '9' {
+			return 0 // Return 0 if a non-digit character is found
+		}
 		result = result*10 + int(char-'0')
 	}
 	return result
