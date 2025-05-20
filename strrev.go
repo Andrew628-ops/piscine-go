9a4ac7face9d8d376e8c63c804f2bb876382da40
aotokpa <andrewokibeotokpa@gmail.com>
2025-05-20 16:21:37 +0100

    my strrev

--- strrev.go
+++ strrev.go
@@ -1,8 +1,8 @@
-package piscine 
+package piscine
 
 func StrRev(s string) string {
 	runes := []rune(s)
-	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1{
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
 		runes[i], runes[j] = runes[j], runes[i]
 	}
 	return string(runes)
