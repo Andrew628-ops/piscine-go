09d90894ac52c5f1bcc5305211e417c7fa84effb
aotokpa <andrewokibeotokpa@gmail.com>
2025-05-23 09:48:51 +0100

    my printcomb

--- printcomb.go
+++ printcomb.go
@@ -1,26 +1,16 @@
 package piscine
 
-import "github.com/01-edu/z01"
+import "fmt"
 
 func PrintComb() {
 	for i := 0; i <= 7; i++ {
 		for j := i + 1; j <= 8; j++ {
 			for k := j + 1; k <= 9; k++ {
-				// Convert digits to rune once, no cast at print time
-				di := '0' + i
-				dj := '0' + j
-				dk := '0' + k
-
-				z01.PrintRune(di)
-				z01.PrintRune(dj)
-				z01.PrintRune(dk)
-
-				if !(i == 7 && j == 8 && k == 9) {
-					z01.PrintRune(',')
-					z01.PrintRune(' ')
+				fmt.Printf("%d%d%d", i, j, k)
+				if i != 7 || j != 8 || k != 9 {
+					fmt.Print(", ")
 				}
 			}
 		}
 	}
-	z01.PrintRune('\n')
 }
