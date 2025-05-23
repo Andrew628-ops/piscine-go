52cbfe1af59870891d6e954c824179cf1036e9f3
aotokpa <andrewokibeotokpa@gmail.com>
2025-05-23 13:31:11 +0100

    my first  iterativefactorial

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -7,7 +7,6 @@ func IterativeFactorial(nb int) int {
 
 	result := 1
 	for i := 2; i <= nb; i++ {
-		// Check for potential overflow by ensuring the result won't exceed max int
 		if result > (1<<31-1)/i {
 			return 0
 		}
