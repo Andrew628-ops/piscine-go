c454f129ca78c79420ab8b2e8962d93261f4d755
aotokpa <andrewokibeotokpa@gmail.com>
2025-05-23 10:16:28 +0100

    my printcomb2

--- printcomb2.go
+++ printcomb2.go
@@ -3,14 +3,11 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintComb2() {
-	for i := rune(0); i <= 98; i++ {
+	for i := 0; i <= 98; i++ {
 		for j := i + 1; j <= 99; j++ {
-			// convert i and j to digits as runes without casting
 			z01.PrintRune('0' + i/10)
 			z01.PrintRune('0' + i%10)
-
 			z01.PrintRune(' ')
-
 			z01.PrintRune('0' + j/10)
 			z01.PrintRune('0' + j%10)
 
