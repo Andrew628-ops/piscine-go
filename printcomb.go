fabefbc94b42d9028c58afe6877b96c62a7d181a
aotokpa <andrewokibeotokpa@gmail.com>
2025-05-23 09:40:46 +0100

    my printcomb

--- printcomb.go
+++ printcomb.go
@@ -6,9 +6,9 @@ func PrintComb() {
 	for i := 0; i <= 7; i++ {
 		for j := i + 1; j <= 8; j++ {
 			for k := j + 1; k <= 9; k++ {
-				z01.PrintRune(i + '0')
-				z01.PrintRune(j + '0')
-				z01.PrintRune(k + '0')
+				z01.PrintRune(rune(i + '0'))
+				z01.PrintRune(rune(j + '0'))
+				z01.PrintRune(rune(k + '0'))
 				if !(i == 7 && j == 8 && k == 9) {
 					z01.PrintRune(',')
 					z01.PrintRune(' ')
