45fcc43469e6103eed08a02c2b4e0a82bd6b6c8a
aotokpa <andrewokibeotokpa@gmail.com>
2025-05-23 09:45:56 +0100

    my printcomb

--- printcomb.go
+++ printcomb.go
@@ -6,9 +6,15 @@ func PrintComb() {
 	for i := 0; i <= 7; i++ {
 		for j := i + 1; j <= 8; j++ {
 			for k := j + 1; k <= 9; k++ {
-				z01.PrintRune(rune(i + '0'))
-				z01.PrintRune(rune(j + '0'))
-				z01.PrintRune(rune(k + '0'))
+				// Convert digits to rune once, no cast at print time
+				di := '0' + i
+				dj := '0' + j
+				dk := '0' + k
+
+				z01.PrintRune(di)
+				z01.PrintRune(dj)
+				z01.PrintRune(dk)
+
 				if !(i == 7 && j == 8 && k == 9) {
 					z01.PrintRune(',')
 					z01.PrintRune(' ')
