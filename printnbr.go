f3c66d45ce28c3cab26997f2b971b37d334f3921
aotokpa <andrewokibeotokpa@gmail.com>
2025-05-23 10:29:45 +0100

    my printnbr

--- printnbr.go
+++ printnbr.go
@@ -4,24 +4,19 @@ import "github.com/01-edu/z01"
 
 func PrintNbr(n int) {
 	if n < 0 {
-		z01.PrintRune('-')
 		if n == -n {
-			printMinInt()
+			z01.PrintRune('-')
+			min := "9223372036854775808"
+			for _, r := range min {
+				z01.PrintRune(r)
+			}
 			return
 		}
+		z01.PrintRune('-')
 		n = -n
 	}
-
 	if n >= 10 {
 		PrintNbr(n / 10)
 	}
 	z01.PrintRune(rune(n%10 + '0'))
-}
-
-func printMinInt() {
-	minIntStr := "-2147483648"
-
-	for _, r := range minIntStr {
-		z01.PrintRune(r)
-	}
 }
