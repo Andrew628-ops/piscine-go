1b1f565f12887d3b3401fc9cad4edde141e06b7e
aotokpa <andrewokibeotokpa@gmail.com>
2025-05-23 12:39:23 +0100

    my iterativefactorial

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -1,5 +1,29 @@
 package piscine
 
+import "github.com/01-edu/z01"
+
+func PrintComb() {
+	for i := 0; i <= 7; i++ {
+		for j := i + 1; j <= 8; j++ {
+			for k := j + 1; k <= 9; k++ {
+				di := '0' + i
+				dj := '0' + j
+				dk := '0' + k
+
+				z01.PrintRune(di)
+				z01.PrintRune(dj)
+				z01.PrintRune(dk)
+
+				if !(i == 7 && j == 8 && k == 9) {
+					z01.PrintRune(',')
+					z01.PrintRune(' ')
+				}
+			}
+		}
+	}
+	z01.PrintRune('\n')
+}
+
 func IterativeFactorial(nb int) int {
 	if nb < 0 {
 		return 0
@@ -7,9 +31,8 @@ func IterativeFactorial(nb int) int {
 
 	result := 1
 	for i := 1; i <= nb; i++ {
-		// Check for overflow before multiplying
-		if result > 0 && (result > (1<<31-1)/i) {
-			return 0 // overflow detected
+		if result > 0 && result > (1<<31-1)/i {
+			return 0
 		}
 		result *= i
 	}
