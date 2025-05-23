8aa8347c9940e04acd64dc5bd9f74cf336b2669b
aotokpa <andrewokibeotokpa@gmail.com>
2025-05-23 10:04:54 +0100

    my printcomb2

--- printcomb2.go
+++ printcomb2.go
@@ -5,20 +5,19 @@ import "github.com/01-edu/z01"
 func PrintComb2() {
 	for i := 0; i <= 98; i++ {
 		for j := i + 1; j <= 99; j++ {
-			printTwoDigit(i)
-			z01.PrintRune(' ')
-			printTwoDigit(j)
+			z01.PrintRune('0' + i/10)
+			z01.PrintRune('0' + i%10)
 
-			if !(i == 98 && j == 99) {
+			z01.PrintRune(' ')
+
+			z01.PrintRune('0' + j/10)
+			z01.PrintRune('0' + j%10)
+
+			if i != 98 || j != 99 {
 				z01.PrintRune(',')
 				z01.PrintRune(' ')
 			}
 		}
 	}
 	z01.PrintRune('\n')
-}
-
-func printTwoDigit(n int) {
-	z01.PrintRune(rune('0' + n/10))
-	z01.PrintRune(rune('0' + n%10))
 }
