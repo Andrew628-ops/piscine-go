563989205559c3e358423869219aa6565a8440bc
aotokpa <andrewokibeotokpa@gmail.com>
2025-05-23 13:54:09 +0100

    my first  iterativefactorial

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -1,17 +1,12 @@
 package piscine
 
 func IterativeFactorial(nb int) int {
-	if nb < 0 || nb > 12 {
+	if nb < 0 || nb > 20 {
 		return 0
 	}
-
 	result := 1
 	for i := 1; i <= nb; i++ {
-		if result > (1<<31-1)/i {
-			return 0
-		}
 		result *= i
 	}
-
 	return result
 }
