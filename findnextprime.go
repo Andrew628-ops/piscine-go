f13690391472cd2fb7d70fd81d954e49eae65cd2
aotokpa <andrewokibeotokpa@gmail.com>
2025-05-23 15:22:27 +0100

    my findnextprime

--- findnextprime.go
+++ findnextprime.go
@@ -4,9 +4,27 @@ func FindNextPrime(nb int) int {
 	if nb <= 2 {
 		return 2
 	}
-	for {
-		if IsPrime(nb) {
+	for candidates := nb; ; candidates++ {
+		if isPrime(candidates) {
+			return candidates
 		}
-		nb++
 	}
+}
+
+func isPrime(n int) bool {
+	if n < 2 {
+		return false
+	}
+	if n == 2 {
+		return true
+	}
+	if n%2 == 0 {
+		return false
+	}
+	for i := 3; i*i <= n; i += 2 {
+		if n%i == 0 {
+			return false
+		}
+	}
+	return true
 }
