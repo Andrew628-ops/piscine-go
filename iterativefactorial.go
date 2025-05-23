854c4fd0f8876d59bd704fb5fccf3c0486c6b023
aotokpa <andrewokibeotokpa@gmail.com>
2025-05-23 12:16:03 +0100

    my iterativefactorial

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -1,11 +1,16 @@
 package piscine
 
 func IterativeFactorial(nb int) int {
-	if nb < 0 || nb > 20 {
+	if nb < 0 {
 		return 0
 	}
+
 	result := 1
 	for i := 1; i <= nb; i++ {
+		// Check for overflow before multiplying
+		if result > 0 && (result > (1<<31-1)/i) {
+			return 0 // overflow detected
+		}
 		result *= i
 	}
 	return result
