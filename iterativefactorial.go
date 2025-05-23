cc53dc105dc46b1b99b0c498e27936e87f8d5e4f
aotokpa <andrewokibeotokpa@gmail.com>
2025-05-23 13:16:43 +0100

    my first  iterativefactorial

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -1,19 +1,16 @@
 package piscine
 
 func IterativeFactorial(nb int) int {
-	// Handle invalid inputs (negative numbers)
 	if nb < 0 {
 		return 0
 	}
 
-	// Factorial of 0 is 1
 	if nb == 0 {
 		return 1
 	}
 
 	result := 1
 	for i := 1; i <= nb; i++ {
-		// Check for overflow before multiplying
 		if result > (1<<63-1)/i {
 			return 0
 		}
