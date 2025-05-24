f57e83c66ee4e9fbe6e5916f7b47042be5d4859a
aotokpa <andrewokibeotokpa@gmail.com>
2025-05-24 14:02:50 +0100

    my printnbrbase

--- printnbrbase.go
+++ printnbrbase.go
@@ -1,8 +1,11 @@
 package piscine
 
-import "github.com/01-edu/z01"
+import (
+	"github.com/01-edu/z01"
+)
 
 func PrintNbrBase(nbr int, base string) {
+	// Validate the base
 	if !isValidBase(base) {
 		z01.PrintRune('N')
 		z01.PrintRune('V')
@@ -11,36 +14,27 @@ func PrintNbrBase(nbr int, base string) {
 
 	baseRunes := []rune(base)
 	baseLen := int64(len(baseRunes))
-
-	var num int64 = int64(nbr)
-	if num == 0 {
-		z01.PrintRune(baseRunes[0])
-		return
-	}
+	num := int64(nbr)
 
 	if num < 0 {
 		z01.PrintRune('-')
-		// Handle the special case of math.MinInt64
-		// Convert to unsigned to prevent overflow
-		printBase(-num, baseRunes, baseLen)
-		return
+		num = -num
 	}
 
 	printBase(num, baseRunes, baseLen)
 }
 
-func printBase(n int64, baseRunes []rune, baseLen int64) {
-	if n >= baseLen {
-		printBase(n/baseLen, baseRunes, baseLen)
+func printBase(num int64, base []rune, baseLen int64) {
+	if num >= baseLen {
+		printBase(num/baseLen, base, baseLen)
 	}
-	z01.PrintRune(baseRunes[n%baseLen])
+	z01.PrintRune(base[num%baseLen])
 }
 
 func isValidBase(base string) bool {
 	if len(base) < 2 {
 		return false
 	}
-
 	seen := make(map[rune]bool)
 	for _, r := range base {
 		if r == '+' || r == '-' || seen[r] {
