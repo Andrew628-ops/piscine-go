6f5422943233b453dae14465899f6c427048bb81
aotokpa <andrewokibeotokpa@gmail.com>
2025-05-25 11:17:46 +0100

    my printprogramname

--- printprogramname/main.go
+++ printprogramname/main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
-	"fmt"
 	"os"
 	"path/filepath"
+
+	"github.com/01-edu/z01"
 )
 
 func main() {
-	programName := filepath.Base(os.Args[0])
-	fmt.Println(programName)
+	progName := filepath.Base(os.Args[0])
+
+	for _, c := range progName {
+		z01.PrintRune(c)
+	}
+	z01.PrintRune('\n')
 }
