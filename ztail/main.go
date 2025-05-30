b0c2a541d4266ecdb61f65788ea53cf6c58a5199
aotokpa <andrewokibeotokpa@gmail.com>
2025-05-30 20:07:34 +0100

    my ztail

--- ztail/main.go
+++ ztail/main.go
@@ -2,18 +2,30 @@ package main
 
 import (
 	"os"
-	"strconv"
 )
+
+func atoi(s string) (int, bool) {
+	n := 0
+	for i := 0; i < len(s); i++ {
+		c := s[i]
+		if c < '0' || c > '9' {
+			return 0, false
+		}
+		n = n*10 + int(c-'0')
+	}
+	return n, true
+}
 
 func main() {
 	args := os.Args[1:]
+
 	if len(args) < 2 || args[0] != "-c" {
 		os.Stderr.WriteString("Usage: go run . -c <number> <file> [file...]\n")
 		os.Exit(1)
 	}
 
-	n, err := strconv.Atoi(args[1])
-	if err != nil || n < 0 {
+	n, ok := atoi(args[1])
+	if !ok {
 		os.Stderr.WriteString("Invalid byte count\n")
 		os.Exit(1)
 	}
@@ -45,7 +57,6 @@ func main() {
 		if start < 0 {
 			start = 0
 		}
-
 		os.Stdout.Write(data[start:])
 	}
 
