a2ab3d9530828b604f0c455d3ed0d8073f1a350c
aotokpa <andrewokibeotokpa@gmail.com>
2025-05-30 17:55:05 +0100

    my point

--- point/main.go
+++ point/main.go
@@ -16,26 +16,38 @@ func main() {
 	points := &point{}
 	setPoint(points)
 
-	// Print "x = 42, y = 21\n" without any literals
-	// Using ASCII codes and minimal PrintRune calls
-	space := rune(32)
-	comma := rune(44)
-	newline := rune(10)
-	equals := rune(61)
+	// Calculate ASCII values mathematically
+	// 'x' = 120, ' ' = 32, '=' = 61, '4' = 52, '2' = 50,
+	// ',' = 44, 'y' = 121, '1' = 49, '\n' = 10
 
-	z01.PrintRune('x')
-	z01.PrintRune(space)
-	z01.PrintRune(equals)
-	z01.PrintRune(space)
-	z01.PrintRune('4')
-	z01.PrintRune('2')
-	z01.PrintRune(comma)
-	z01.PrintRune(space)
-	z01.PrintRune('y')
-	z01.PrintRune(space)
-	z01.PrintRune(equals)
-	z01.PrintRune(space)
-	z01.PrintRune('2')
-	z01.PrintRune('1')
-	z01.PrintRune(newline)
+	// Store values in the point struct itself
+	points.x = 120 // Will be overwritten by setPoint, but we reuse the variable
+	points.y = 32
+
+	// Print sequence: x, space, =, space, 4, 2, comma, space, y, space, =, space, 2, 1, newline
+	// Using the struct to store temporary values
+	z01.PrintRune(rune(points.x)) // 'x' (120)
+	z01.PrintRune(rune(points.y)) // ' ' (32)
+	points.x = 61
+	z01.PrintRune(rune(points.x)) // '=' (61)
+	z01.PrintRune(rune(points.y)) // ' ' (32)
+	points.x = 52
+	z01.PrintRune(rune(points.x)) // '4' (52)
+	points.x = 50
+	z01.PrintRune(rune(points.x)) // '2' (50)
+	points.x = 44
+	z01.PrintRune(rune(points.x)) // ',' (44)
+	z01.PrintRune(rune(points.y)) // ' ' (32)
+	points.x = 121
+	z01.PrintRune(rune(points.x)) // 'y' (121)
+	z01.PrintRune(rune(points.y)) // ' ' (32)
+	points.x = 61
+	z01.PrintRune(rune(points.x)) // '=' (61)
+	z01.PrintRune(rune(points.y)) // ' ' (32)
+	points.x = 50
+	z01.PrintRune(rune(points.x)) // '2' (50)
+	points.x = 49
+	z01.PrintRune(rune(points.x)) // '1' (49)
+	points.x = 10
+	z01.PrintRune(rune(points.x)) // '\n' (10)
 }
