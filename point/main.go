a0840e9fc30efe83bf5c8358975e593d56bbf3f6
aotokpa <andrewokibeotokpa@gmail.com>
2025-05-30 17:26:40 +0100

    my point

--- point/main.go
+++ point/main.go
@@ -12,36 +12,12 @@ func setPoint(ptr *point) {
 	ptr.y = 21
 }
 
-func printDigit(n int) {
-	if n >= 0 && n <= 9 {
-		z01.PrintRune('0' + n)
-	}
-}
-
 func main() {
 	points := &point{}
 	setPoint(points)
 
-	z01.PrintRune('x')
-	z01.PrintRune(' ')
-	z01.PrintRune('=')
-	z01.PrintRune(' ')
-
-	// Print 42 (x)
-	z01.PrintRune('4')
-	z01.PrintRune('2')
-
-	z01.PrintRune(',')
-	z01.PrintRune(' ')
-
-	z01.PrintRune('y')
-	z01.PrintRune(' ')
-	z01.PrintRune('=')
-	z01.PrintRune(' ')
-
-	// Print 21 (y)
-	z01.PrintRune('2')
-	z01.PrintRune('1')
-
-	z01.PrintRune('\n')
+	message := "x = 42, y = 21\n"
+	for _, r := range message {
+		z01.PrintRune(r)
+	}
 }
