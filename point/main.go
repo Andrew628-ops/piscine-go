d376cab5153b64a8c0f978e34091f5bcee1c9094
aotokpa <andrewokibeotokpa@gmail.com>
2025-05-30 17:19:28 +0100

    my point

---
+++ point/main.go
@@ -0,0 +1,49 @@
+package main
+
+import "github.com/01-edu/z01"
+
+type point struct {
+	x int
+	y int
+}
+
+func setPoint(ptr *point) {
+	ptr.x = 42
+	ptr.y = 21
+}
+
+func printStr(s string) {
+	for _, r := range s {
+		z01.PrintRune(r)
+	}
+}
+
+func printNbr(n int) {
+	if n == 0 {
+		z01.PrintRune('0')
+		return
+	}
+	if n < 0 {
+		z01.PrintRune('-')
+		n = -n
+	}
+	digits := []rune{}
+	for n > 0 {
+		digits = append(digits, rune(n%10)+'0')
+		n /= 10
+	}
+	for i := len(digits) - 1; i >= 0; i-- {
+		z01.PrintRune(digits[i])
+	}
+}
+
+func main() {
+	points := &point{}
+	setPoint(points)
+
+	printStr("x = ")
+	printNbr(points.x)
+	printStr(", y = ")
+	printNbr(points.y)
+	z01.PrintRune('\n')
+}
