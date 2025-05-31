be055efb3d801d0e87ccd9f8158a8a6fdba5d725
aotokpa <andrewokibeotokpa@gmail.com>
2025-05-31 14:31:18 +0100

    my doop

--- doop/main.go
+++ doop/main.go
@@ -2,6 +2,9 @@ package main
 
 import "os"
 
+const MaxInt64 = 9223372036854775807
+const MinInt64 = -9223372036854775808
+
 func main() {
 	if len(os.Args) != 4 {
 		return
@@ -18,13 +21,25 @@ func main() {
 		return
 	}
 
-	var result int
+	var result int64
 	switch op {
 	case "+":
+		if (b > 0 && a > MaxInt64-b) || (b < 0 && a < MinInt64-b) {
+			return
+		}
 		result = a + b
 	case "-":
+		if (b < 0 && a > MaxInt64+b) || (b > 0 && a < MinInt64+b) {
+			return
+		}
 		result = a - b
 	case "*":
+		if (a > 0 && b > 0 && a > MaxInt64/b) ||
+			(a > 0 && b < 0 && b < MinInt64/a) ||
+			(a < 0 && b > 0 && a < MinInt64/b) ||
+			(a < 0 && b < 0 && a < MaxInt64/b) {
+			return
+		}
 		result = a * b
 	case "/":
 		if b == 0 {
@@ -45,12 +60,12 @@ func main() {
 	os.Stdout.Write([]byte(Itoa(result) + "\n"))
 }
 
-func Atoi(s string) (int, bool) {
+func Atoi(s string) (int64, bool) {
 	if len(s) == 0 {
 		return 0, false
 	}
 
-	sign := 1
+	sign := int64(1)
 	i := 0
 	if s[0] == '-' {
 		sign = -1
@@ -59,17 +74,21 @@ func Atoi(s string) (int, bool) {
 		i++
 	}
 
-	num := 0
+	var num int64 = 0
 	for ; i < len(s); i++ {
 		if s[i] < '0' || s[i] > '9' {
 			return 0, false
 		}
-		num = num*10 + int(s[i]-'0')
+		digit := int64(s[i] - '0')
+		if num > (MaxInt64-digit)/10 {
+			return 0, false // overflow during parsing
+		}
+		num = num*10 + digit
 	}
 	return num * sign, true
 }
 
-func Itoa(n int) string {
+func Itoa(n int64) string {
 	if n == 0 {
 		return "0"
 	}
