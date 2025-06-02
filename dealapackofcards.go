c21378b2b5f291b28e46247a57fbda34e1f359dc
aotokpa <andrewokibeotokpa@gmail.com>
2025-06-02 19:16:37 +0100

    my dealapackofcards.go

--- dealapackofcards.go
+++ dealapackofcards.go
@@ -1,28 +1,44 @@
 package piscine
 
-import (
-	"fmt"
-
-	"github.com/01-edu/z01"
-)
+import "github.com/01-edu/z01"
 
 func DealAPackOfCards(deck []int) {
 	for i := 0; i < 4; i++ {
-		playerHeader := fmt.Sprintf("Player %d: ", i+1)
-		for _, r := range playerHeader {
-			z01.PrintRune(r)
-		}
-
+		printString("Player ")
+		z01.PrintRune('1' + rune(i))
+		printString(": ")
 		for j := 0; j < 3; j++ {
-			card := fmt.Sprintf("%d", deck[i*3+j])
-			for _, r := range card {
-				z01.PrintRune(r)
-			}
+			printNumber(deck[i*3+j])
 			if j < 2 {
 				z01.PrintRune(',')
 				z01.PrintRune(' ')
 			}
 		}
 		z01.PrintRune('\n')
+	}
+}
+
+func printString(s string) {
+	for _, r := range s {
+		z01.PrintRune(r)
+	}
+}
+
+func printNumber(n int) {
+	if n == 0 {
+		z01.PrintRune('0')
+		return
+	}
+	if n < 0 {
+		z01.PrintRune('-')
+		n = -n
+	}
+	var digits []rune
+	for n > 0 {
+		digits = append([]rune{'0' + rune(n%10)}, digits...)
+		n /= 10
+	}
+	for _, r := range digits {
+		z01.PrintRune(r)
 	}
 }
