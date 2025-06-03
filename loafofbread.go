58d25caccca0f84a692deb58ae1782409e522fef
aotokpa <andrewokibeotokpa@gmail.com>
2025-06-03 10:05:37 +0100

    loaf of bread

--- loafofbread.go
+++ loafofbread.go
@@ -11,6 +11,11 @@ func LoafOfBread(str string) string {
 
 	for _, char := range str {
 		if char == ' ' {
+			if count < 5 {
+				continue // Skip spaces when building 5-char groups
+			}
+			// Keep space if it appears after a complete group
+			result += string(char)
 			continue
 		}
 
