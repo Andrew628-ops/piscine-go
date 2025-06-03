a3c0d59083eb889b09faecaa487046234c066523
aotokpa <andrewokibeotokpa@gmail.com>
2025-06-03 04:28:10 +0100

    my podiumposition

--- podiumposition.go
+++ podiumposition.go
@@ -2,14 +2,8 @@ package piscine
 
 func PodiumPosition(podium [][]string) [][]string {
 	length := len(podium)
-	var result [][]string
-
-	// Create a slice of correct length by assigning manually
-	result = podium
-
 	for i := 0; i < length/2; i++ {
-		// Swap elements
 		podium[i], podium[length-1-i] = podium[length-1-i], podium[i]
 	}
-	return result
+	return podium
 }
